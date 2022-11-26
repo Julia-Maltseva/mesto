5ca89c902d686fd036d5edf1e29d326470074ae1
Julia Maltseva <yuliya-maltseva93@yandex.ru>
2022-11-26 13:59:11 +0300

    Практическая работа 7

---
+++ scripts/Card.js
@@ -0,0 +1,62 @@
+import { openPopup, imageBigPhoto, nameBigPhoto, popupShowPhoto, closePopupByEsc, closePopup } from './variables.js';
+
+export default class Card {
+  constructor(data, templateSelector, popupShowPhoto) {
+    this._name = data.name;
+    this._link = data.link;
+    this._templateSelector = templateSelector;
+    this._popupShowPhoto = popupShowPhoto;
+  }
+
+  _getTemplate() {
+    const cardElement = document
+    .querySelector(this._templateSelector)
+    .content
+    .querySelector('.element')
+    .cloneNode(true);
+
+    return cardElement;
+  }
+
+  generateCard() {
+    this._element = this._getTemplate();
+    this._setEventListeners();
+
+    this._element.querySelector('.element__title').textContent = this._name;
+    this._element.querySelector('.element__image').src = this._link;
+    this._element.querySelector('.element__image').alt = this._name;
+
+    return this._element;
+  }
+
+  _setEventListeners() {
+    this._element.querySelector('.element__delete-button').addEventListener('click', () => {
+      this._deleteElement();
+    });
+
+    this._element.querySelector('.element__image').addEventListener('click', () => {
+      this._showPhoto();
+    });
+
+    this._element.querySelector('.element__like-button').addEventListener('click', () => {
+      this._toggleLike();
+    });
+  }
+
+  _deleteElement() {
+    this._element.remove();
+  }
+
+  _showPhoto() {
+    openPopup(popupShowPhoto);
+    nameBigPhoto.textContent = this._name;
+    imageBigPhoto.src = this._link;
+    imageBigPhoto.alt = this._name;
+}
+
+_toggleLike() {
+  this._element.querySelector('.element__like-button')
+  .classList.toggle('element__like-button_active');
+}
+}
+
--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,5 @@
 const popupEditProfile = document.querySelector('.popup_type_edit')
 const popupAddCard = document.querySelector('.popup_type_add-card')
-const popupShowPhoto = document.querySelector('.popup_type_show-photo')
 const popup = document.querySelector('.popup')
 const buttonEditProfile = document.querySelector('.profile__edit-button')
 const buttonClose = document.querySelector('.popup__close-button')
@@ -12,21 +11,8 @@ const userName = document.querySelector('.profile__name')
 const userJob = document.querySelector('.profile__job')
 const cards = document.querySelector('.elements')
 const popupFormAdd = document.querySelector('.popup__form_add')
-const nameBigPhoto = document.querySelector('.popup__photo-title')
-const imageBigPhoto = document.querySelector('.popup__photo')
 const popupFirstFieldAdd = document.querySelector('.popup__first-field_add')
 const popupSecondFieldAdd = document.querySelector('.popup__second-field_add')
-const template = document.querySelector('#template-card')
-
-function openPopup(popup) {
-  popup.classList.add('popup_opened')
-  document.addEventListener('keydown', closePopupByEsc)
-}
-
-function closePopup(popup) {
-  popup.classList.remove('popup_opened')
-  document.removeEventListener('keydown', closePopupByEsc)
-}
 
 buttonEditProfile.addEventListener('click', () => {
   openPopup(popupEditProfile)
@@ -50,6 +36,19 @@ function submitFormHandler (evt) {
   closePopup(popup)
   userName.textContent = popupFirstFieldEdit.value;
   userJob.textContent = popupSecondFieldEdit.value;
+}
+
+import Card from './Card.js'
+import FormValidator from './FormValidator.js'
+
+const settings = {
+  formElement: '.popup__form',
+  inputElement: '.popup__input',
+  submitButtonElement: '.popup__save-button',
+  inactiveButtonClass: 'popup__save-button_invalid',
+  activeButtonClass: 'popup__save-button_valid',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'error'  
 }
 
 const initialCards = [
@@ -81,55 +80,34 @@ const initialCards = [
 
 const render = () => {
   initialCards.forEach((item) => {
-    const card = createCard(item.name, item.link);
-    cards.append(card)
+    const card = createCard(item);
+    cards.append(card);
   });
 } 
 
-function createCard(name, link) {
-  const card = template.content.cloneNode(true)
-  const elementTitle = card.querySelector('.element__title')
-  elementTitle.textContent = name;
-  const elementImage = card.querySelector('.element__image')
-  elementImage.src = link;
-  elementImage.alt = name;
-  const buttonLike = card.querySelector('.element__like-button')
-  const buttonDelete = card.querySelector('.element__delete-button')
+function createCard(data) {
+  const card = new Card(data, '.template-card');
+  const cardElement = card.generateCard();
 
-  buttonLike.addEventListener('click', (evt) => {
-    evt.target.classList.toggle('element__like-button_active');
-  });
-
-  elementImage.addEventListener('click', showPhoto)
-  buttonDelete.addEventListener('click', deleteElement)
-
-  function showPhoto(evt) {
-    const elementImage = evt.target.closest('.element__image');
-    openPopup(popupShowPhoto)
-    nameBigPhoto.textContent = elementTitle.textContent;
-    imageBigPhoto.src = elementImage.src;
-    imageBigPhoto.alt = elementImage.textContent;
-  }
-
-  return card
-  
+  return cardElement;
 } 
 
 render();
 
 function submitFormAdd (evt) {
   evt.preventDefault()
-  const item = createCard(popupFirstFieldAdd.value, popupSecondFieldAdd.value);
+  const item = createCard({name: popupFirstFieldAdd.value, link: popupSecondFieldAdd.value});
   cards.prepend(item)
   closePopup(popupAddCard);
   popupFormAdd.reset();
   inactiveButtonClass();
 }
 
-function deleteElement(evt) {
-  const card = evt.target.closest('.element');
-  card.remove();
-}
+const formValidEdit = new FormValidator(settings, popupFormEdit);
+const formValidAdd = new FormValidator(settings, popupFormAdd);
+
+formValidEdit.enableValidation();
+formValidAdd.enableValidation();
 
 const popups = document.querySelectorAll('.popup')
 popups.forEach((popup) => {
@@ -143,13 +121,7 @@ popups.forEach((popup) => {
   })  
 })
 
-const closePopupByEsc = (evt) => {
-  if(evt.key === 'Escape') {
-    const openedPopup = document.querySelector('.popup_opened');
-    closePopup(openedPopup)
-  }
-}
-
 popupFormEdit.addEventListener('submit', submitFormHandler)
 popupFormAdd.addEventListener('submit', submitFormAdd)
 
+import { openPopup, closePopupByEsc, closePopup } from './variables.js';
